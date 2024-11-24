a68eba089f99b1126ac4f16c1a5e965c0cb27bc7
yoni <johnysalvadorcampos@gmail.com>
2024-11-23 20:57:54 -0600

    Actualización: componentes de ciudad y usuario a estáticos

--- src/app/components/nuevaciudad/nuevaciudad.component.ts
+++ src/app/components/nuevaciudad/nuevaciudad.component.ts
@@ -1,46 +1,20 @@
-/*import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevaciudad',
   standalone: true,
-  imports: [],
-  templateUrl: './nuevaciudad.component.html',
-  styleUrl: './nuevaciudad.component.css'
-})
-export class NuevaciudadComponent {
-
-}*/
-import { Component } from '@angular/core';
-import { CiudadService } from '../../services/ciudad.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-nuevousuario',
-  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './nuevaciudad.component.html',
-  styleUrl: './nuevaciudad.component.css'
+  styleUrls: ['./nuevaciudad.component.css']
 })
 export class NuevaciudadComponent {
-  nuevaCiudad: any = {}; // Objeto para el nuevo usuario
-
-  constructor(private ciudadService: CiudadService, private router: Router) { }
+  nuevaCiudad: any = {}; // Objeto para el nuevo registro
 
   crearCiudad() {
-    console.log('Creando nuevo registro:', this.nuevaCiudad);
-    this.ciudadService.postCiudad(this.nuevaCiudad).subscribe(
-      (res) => {
-        console.log('Ciudad creada:', res);
-        this.nuevaCiudad = { id_ciudad: '' }; // Reinicia el formulario
-        this.router.navigate(['/ciudad']); // Redirige a la lista de registros
-      },
-      (err) => {
-        console.error('Error al crear el registro:', err);
-      }
-    );
+    console.log('Datos de la nueva ciudad:', this.nuevaCiudad);
+    alert('Ciudad creada exitosamente: ' + JSON.stringify(this.nuevaCiudad));
+    this.nuevaCiudad = {}; // Reinicia el formulario
   }
-
 }
-
